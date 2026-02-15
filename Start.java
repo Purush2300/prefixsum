3584ea828972d3e959b72e442011435792283ebf
purush2300 <reddypurushothm@gmail.com>
2026-02-16 00:52:31 +0530

    Arrays

--- Start.java
+++ Start.java
@@ -1,18 +1,24 @@
 public class Start {
     public static void main(String[] args) {
         int []a={-3,2,-3,4,2};
-        int st=1;
-        for (int i = 0; i < a.length; i++) {
-            
-            int temp=st;
-            int sum=temp+a[i];
-          
-            if(sum>0){
-               st=sum;
-               System.out.println(sum);
-            }
-
+        // int[] prefixsum=new int[a.length+1];
+        // // prefixsum[0]=a[0];
+        // prefixsum[0]=0;
+        // for (int i = 1; i < prefixsum.length; i++) {
+        //     prefixsum[i]=prefixsum[i-1]+a[i];
+        // }
+        // for (int i = 1; i < prefixsum.length; i++) {
+        //     prefixsum[i]=prefixsum[i-1]+a[i-1];
+        // }
+        a[0]=a[0];
+        for (int i = 1; i < a.length; i++) {
+            a[i]=a[i]+a[i-1];
+           
         }
+        for (int i = 0; i < a.length; i++) {
+             System.out.println(a[i]);
+        }
+        
     
       
     }
